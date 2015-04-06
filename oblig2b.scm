7e91f8a606c7438271fd0c66c9f0706c3ef68195
thoschwi <thoschwi@student.matnat.uio.no>
2015-04-06 23:17:17 +0200

    2a

--- oblig2b.scm
+++ oblig2b.scm
@@ -3,6 +3,8 @@
 ;; 1. Innkapsling, lokal tilstand og omgivelsesmodellen
 ;; a) 
 
+(define count 42)
+
 (define (make-counter)
   (let ((count 0))
     (lambda ()
@@ -10,13 +12,28 @@
         (set! count (+ count 1))
         count))))
 
-(define count 42)
-
 (define c1 (make-counter))
-
 (define c2 (make-counter))
 
 ;; b) Se vedlagt "1b-omgivelse.png"
 
+
+;; 2 Innkapsling, lokal tilstand og message passing.
+;; a)
+
+(define (make-stack init)
+  (let ((stack init))
+               
+    (lambda (message . items) ;; Items er en argumentliste av variabel lengde.
+      (case message
+        ('pop! (if (null? stack)
+                   '() ;; Aner ikke hvordan man bare gjor "ingenting"...
+                   (set! stack (cdr stack))))
+        ('push! (set! stack (append (reverse items) stack)))
+        ('stack stack)
+        (else "Invalid message!")))))
+
+(define s1 (make-stack (list 'foo 'bar)))
+(define s2 (make-stack '()))
 
 
