2d9a5c23c8311f3e54b3fbe08a57661fc3a9822b
thoschwi <thoschwi@student.matnat.uio.no>
2015-04-10 17:13:41 +0200

    Merge

--- oblig2b.scm
+++ oblig2b.scm
@@ -36,14 +36,110 @@
 (define s1 (make-stack (list 'foo 'bar)))
 (define s2 (make-stack '()))
 
-;; b) 
+
+;; b)
+
+(define (push!)
+  (lambda (stack . items)
+    (set! stack (append (reverse items) stack))))
+
+(push! s1 'foo 'faa)
+
+(define (pop!)
+  (lambda (items)
+    ('pop! (if (null? items)
+           '()
+           (set! items (cdr items))))))
 
 ;; 3 Strukturdeling og sirkulære lister
 ;; a)
 
+(define bar (list 'a 'b 'c 'd 'e))
+(set-cdr! (cdddr bar) (cdr bar))
 ;; Se vedlagt "3a-boks-peker.png".
-;; Returverdiene til list-ref kommer av at listen er sirkulaer. (list-ref bar 5) 
-;; og oppover ville ikke vaert gyldige indekser i den opprinnelige, asykliske 
-;; listen, men her starter den "paa nytt". Elementet "e" finnes ikke lenger fordi
-;; set-cdr! destruktivt satte (b c d)(som naa ikke er terminert, derav det 
-;; sirkulaere resultatet) der istedet.
+;; Returverdiene til list-ref kommer av at listen er blitt sirkulaer etter 
+;; kallet paa set-cdr!. (list-ref bar 5) og oppover ville ikke vaert gyldige
+;; indekser i den opprinnelige, asykliske listen, men her starter den "paa nytt".
+;; Elementet "e" finnes ikke lenger fordi set-cdr! satte (b c d) der istedet.
+
+
+;; b)
+
+(define bah (list 'bring 'a 'towel))
+(set-car! bah (cdr bah))
+;; Se vedlagt "3b-boks-peker.png".
+;; I det forste kallet paa set-car!, settes car og cdr til 
+;; aa peke paa ett og samme OBJEKT (bevist ved eq?). Naar
+;; vi etterpaa kaller set-car! paa car, blir det forste elementet
+;; likt i begge "instanser" av listen paa utskriften.
+
+
+;; c)
+
+(define (cycle? items) ;; Konstant minnebruk.
+  (define (race tortoise hare)
+    (cond ((null? hare) #f)
+          ((null? (cdr hare)) #f)
+          ((eq? tortoise hare) #t)
+          (else (race (cdr tortoise) (cddr hare)))))
+  (race items (cdr items)))
+
+;;(cycle? '(hey ho))
+;;(cycle? bar)
+
+;; d)
+
+;; Lister har pr definisjon en endelig lengde og er terminert av den
+;; tomme listen (null). Sykliske "lister" er aapenbart ikke terminert.
+;; list? detekterer tydeligvis sykluser i tillegg til enkeltsymboler.
+
+;; e)
+
+;; Vi definerer en ring for aa vaere en lenket liste av tripler som alle har
+;; pekere til baade det forrige og neste elementet i listen.
+;; En triple er en liste med 3 elementer som har en verdi som forste element, og 
+;; pekere til venstre og hoyre triple som henholdsvis 2 og 3 element. Forste
+;; triple (top) sin venstre peker paa triplet som inneholder det siste elementet
+;; i listen som man startet med. Tilsvarende peker det siste triplet sin hoyre
+;; til top sin venstre.
+
+;;Abstraksjonsbarriere 
+;;Konstruktor
+(define (make-triple value left right)
+  (list value left right))
+;;Selektorer
+(define (value triple)
+  (car triple))  
+(define (left triple)
+  (cdr triple))
+(define (right triple)
+  (cddr triple))
+
+(define (make-ring items)
+  (define top (make-triple (car items)'o 'o));;o er plassholder
+  (define (o? symbol)
+    (if (equal? symbol 'o)
+        #t
+        #f))  
+
+  (define (build-triples items triple)
+    (cond ((o? (right top)) (build-triples (cdr items)
+                                (make-triple (cadr items) top 'o)))
+          ;; Her sluttes ringen...
+          ((null? items) (begin
+                           (set! triple (left top)) 
+                           (set! top (right triple))
+                           top))
+          (else (build-triples (cdr items)
+                               (make-triple (car items) triple 'o)))))
+  
+  (let ((first (build-triples items top)))  
+    (lambda ()
+      first)))
+
+(define (top ring)
+  ())
+
+(define r1 (make-ring '(1 2 3 4)))
+
+;; f)
