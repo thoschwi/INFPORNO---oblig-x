9b0186d3e0dd7b0506aabeabb0c2dda5c9c81b7c
thoschwi <thoschwi@student.matnat.uio.no>
2015-04-26 21:09:15 +0200

    Tried something new... didn't quite work

--- Oblig3/a/Oblig3a.scm
+++ Oblig3/a/Oblig3a.scm
@@ -4,26 +4,38 @@
 ;;1. PROSEDYRER FOR BEDRE PROSEDYRER
 
 ;;a - b)
+;;(define (mem message proc);; Den gamle.
+  
+;;  (define (memoize f)
+  ;;  (let ((table (make-table)))
+    ;;  (lambda x
+      ;;  (let ((previously-computed-result
+        ;;       (lookup x table)))
+          ;;(or previously-computed-result
+            ;;  (let ((result (apply f x)))
+              ;;  (insert! x result table)
+                ;;result))))))
+  
+  ;;(let ((original-proc proc))
+    ;;  (case message
+      ;;  ('memoize (memoize proc))
+        ;;('unmemoize original-proc))))) ;; Unmemoize funker ikke.
+
 (define (mem message proc)
   
   (define (memoize f)
-    (let ((table (make-table)))
-      ;; Memoizer-koden fra SICP oppgave 3.27. Fungerer ikke med 
-      ;; prosedyrer som tar inn vilkaarlig antall argumenter.
-      (lambda x
-        (let ((previously-computed-result
-               (lookup x table)))
-          (or previously-computed-result
-              (let ((result (apply f x)))
-                (insert! x result table)
-                result))))))
-  
-  (define (store-proc proc)
-    (lambda args 
-      (apply proc args)))
-  
-  (let ((original-proc (store-proc proc)))
-    (case message
-      ('memoize (memoize proc))
-      ('unmemoize original-proc)))) ;; Unmemoize funker ikke.
-
+    (let ((table (make-table))
+          (original-proc proc))
+          (lambda x
+            (case message 
+              ('memoize 
+               (let ((previously-computed-result
+                      (lookup x table)))
+                 (or previously-computed-result
+                     (let ((result (apply f x)))
+                       (insert! x result table)
+                       result))))
+               ('umemoize
+                original-proc)))))
+            
+            (memoize proc))
