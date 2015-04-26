b240ed2a77d466a936c26d60f59d93a1e24fd2a9
thoschwi <thoschwi@student.matnat.uio.no>
2015-04-26 19:09:03 +0200

    Restructured repo + added 3a

---
+++ Oblig3/a/Oblig3a.scm
@@ -0,0 +1,26 @@
+;; Oblig3a.scm av Benjamin A. Thomas (benjamat) og Thomas Schwitalla (thoschwi)
+(load "prekode3a.scm")
+
+;;1. PROSEDYRER FOR BEDRE PROSEDYRER
+
+;;a)
+(define (mem message proc)
+      
+    (define (memoize f)
+      (let ((table (make-table)))
+        ;; Memoizer-koden fra SICP oppgave 3.27. Fungerer ikke med 
+        ;; prosedyrer som tar inn vilkaarlig antall argumenter.
+        (lambda (x) 
+          (let ((previously-computed-result
+                 (lookup x table)))
+            (or previously-computed-result
+                (let ((result (f x)))
+                  (insert! x result table)
+                  result))))))
+  
+     (let ((original-proc proc))
+       (case message
+         ('memoize (memoize proc))
+         ('unmemoize original-proc)))) ;; Unmemoize funker ikke.
+    
+    
