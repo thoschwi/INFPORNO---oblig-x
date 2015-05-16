85a21416f60e42084d6ac0096eeca3edeaeef51d
thoschwi <thoschwi@student.matnat.uio.no>
2015-05-16 15:26:14 +0200

    3a) werks.

--- Oblig3/b/oblig3b.scm
+++ Oblig3/b/oblig3b.scm
@@ -2,7 +2,7 @@
 
 (load "evaluator.scm")
 (define (repl)
-  (read-eval-print-loop));; Ble en smule lei av å skrive denne setningen...
+  (read-eval-print-loop))
 
 (set! the-global-environment (setup-environment))
 
@@ -32,9 +32,10 @@
 ;; til innenfor hvert enkelt miljø, der det inneværende miljøets bindinger gjelder først.
 ;; I det globale miljøet har man to cond'er: spesialformen og variablen vi definerte 
 ;; til å være 3. I foo sitt miljø har man også en "cond", men denne er en lokal variabel. 
-;; Spesialformen cond gjelder når man kaller den med riktig syntaks (som en prosedyre). 
-;; Det samme gjelder for prosedyrene "else" og "square". Meta-REPL'en fungerer
-;; med andre ord som standard REPL'en.
+;; Spesialformen cond gjelder når man kaller den som en prosedyre (den andre cond er
+;; selvevaluerende og vil tolkes deretter). 
+;; Det samme gjelder for prosedyrene "else" og "square". Meta-REPLet fungerer
+;; med andre ord som standard REPLet.
 
 
 ;;2. PRIMITIVER/INNEBYGDE PROSEDYRER
@@ -65,8 +66,88 @@
 
 ;;b)
 
+;; Tolker oppgaven slik at install-primitive! skal kunne kjøres kun fra scheme-REPLet
+;; og IKKE meta-REPLet (oppgaveteksten er uklar her).
+
 (define (install-primitive! name exp)
   (let ((new-primitive (list (list name exp))))
-    (set! primitive-procedures 
+    (set! primitive-procedures ;;Legger til exp i listen over primitiver for testformål.
           (append primitive-procedures new-primitive))
     (define-variable! name (list 'primitive exp) the-global-environment)))
+
+
+;;3. NYE SPECIAL FORMS OG ALTERNATIV SYNTAKS
+;;a)
+
+;;Kopi
+(define (eval-special-form exp env)
+  (cond ((quoted? exp) (text-of-quotation exp))
+        ((assignment? exp) (eval-assignment exp env))
+        ((definition? exp) (eval-definition exp env))
+        ((if? exp) (eval-if exp env))
+        ((lambda? exp)
+         (make-procedure (lambda-parameters exp)
+                         (lambda-body exp)
+                         env))
+        ((begin? exp) 
+         (eval-sequence (begin-actions exp) env))
+        ((cond? exp) (mc-eval (cond->if exp) env))
+        ;;3a) And og Or:
+        ((and? exp)(eval-and exp env))
+        ((or? exp)(eval-or exp env))))
+
+;;Kopi
+(define (special-form? exp)
+  (cond ((quoted? exp) #t)
+        ((assignment? exp) #t)
+        ((definition? exp) #t)
+        ((if? exp) #t)
+        ((lambda? exp) #t)
+        ((begin? exp) #t)
+        ((cond? exp) #t)
+        ;;3a) And og Or:
+        ((and? exp) #t)
+        ((or? exp) #t)
+        (else #f)))
+
+(define (and? exp)(tagged-list? exp 'and))
+
+(define (eval-and exp env)
+  ;; Egne selektorer for lesbarhetens skyld:
+  (define (and-first exps)
+    (car exps))
+  (define (and-rest exps)
+    (cdr exps))  
+  ;; Evaluerer ett og ett uttrykk helt til noe blir false, eller til vi er gått 
+  ;; tom for operander:
+  (define (eval-ops exps)
+    (if (null? exps) 
+        'true
+        (let ((first (mc-eval (and-first exps) env))
+              (rest (and-rest exps)))
+          (if (false? first)
+              'false
+              (if (no-operands? rest)
+                  first
+                  (eval-ops rest))))))
+  (eval-ops (and-rest exp)))
+  
+
+(define (or? exp)(tagged-list? exp 'or))
+
+(define (eval-or exp env)
+  (define (first-or exps)
+    (car exps))
+  (define (or-rest exps)
+    (cdr exps))
+  
+  (define (eval-ops exps)
+    (if (null? exps)
+        'false
+        (let ((first (mc-eval (first-or exps) env))
+              (rest (or-rest exps)))
+          (if (true? first)
+              first
+              (eval-ops rest)))))
+  (eval-ops (or-rest exp)))
+
